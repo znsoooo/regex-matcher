5642d67b5db03f7ab37b081f07d2688b8ddbce03
Shixian Li <lsx7@sina.com>
2023-06-24 08:01:04 +0800

    Chore: Build as onedir mode and move dlls into sub-folder

---
+++ dist.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set name=RegexMatcher
+set root=dist\%name%
+set libs=%root%\wx
+
+pyinstaller -ywsF --noupx %name%.py
+echo.
+
+pyinstaller -yws --noupx %name%.py
+echo.
+
+del  %root%\%name%.exe.manifest
+echo.
+
+move %root%\*.pyd %libs%
+move %root%\*.dll %libs%
+move %libs%\python*.dll %root%
+echo.
+
+%root%\%name%.exe
+pause
